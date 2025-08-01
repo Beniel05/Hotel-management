dc0f38f6e8542eea0b70a564d52b2d03e6980477
beniel05 <benielebenezer@gmail.com>
2025-08-01 21:38:05 +0530

    Deleted all

--- Payment.java
+++
@@ -1,7 +0,0 @@
-public class Payment {
-    public static boolean processPayment(String customerName, double amount) {
-        System.out.println("Processing payment for " + customerName + ": ₹" + amount);
-        // Simulated success
-        return true;
-    }
-}
